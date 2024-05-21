68d61eb34fae65a282ebe61ab4f16d716041b2e9
ben-hawks <ben.hawks97@gmail.com>
2024-05-20 22:08:41 -0500

    First pass at adding board def of AIV DC32 Badge

---
+++ ports/raspberrypi/boards/aivillage_dc32_badge/aivillage-badge-shared.h
@@ -0,0 +1,14 @@
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2021 Scott Shawcroft for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+#ifndef AIVILLAGE_DC32_BADGE_SHARED
+#define AIVILLAGE_DC32_BADGE_SHARED
+
+#include "shared-bindings/digitalio/DigitalInOut.h"
+
+extern digitalio_digitalinout_obj_t enable_pin_obj;
+
+#endif // AIVILLAGE_DC32_BADGE_SHARED
